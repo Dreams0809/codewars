aafc7b776cae4b37dea433231ef104b81d181471
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2026-05-06 12:11:41 -0400

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-grade-book/grasshopper-grade-book.py
@@ -0,0 +1,14 @@
+def get_grade(s1, s2, s3):
+    avg = (s1 +s2 + s3) / 3
+    
+    if avg >= 90:
+        return 'A'
+    if avg >= 80:
+        return 'B'
+    if avg >= 70:
+        return 'C'
+    if avg >= 60:
+        return 'D'
+    if avg < 60:
+        return 'F'
+    
