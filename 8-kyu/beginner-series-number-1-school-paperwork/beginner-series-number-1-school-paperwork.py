dc5e65fa81298d226354217ad8017dcb834ad1d1
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2026-05-11 12:32:25 -0400

    Add solution - CodeHub

---
+++ 8-kyu/beginner-series-number-1-school-paperwork/beginner-series-number-1-school-paperwork.py
@@ -0,0 +1,10 @@
+def paperwork(n, m):
+    # Happy Coding! ^_^
+# #     if n and m > 0:
+# #         return n * m
+#     if n or m < 0:
+#         return 0
+    if n < 0 or m < 0:
+        return 0
+    return n * m
+​
