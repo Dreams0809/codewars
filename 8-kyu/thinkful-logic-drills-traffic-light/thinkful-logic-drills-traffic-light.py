4f82429f8ab2fa13a3328a137a213995a08c598c
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2026-05-12 11:11:56 -0400

    Add solution - CodeHub

---
+++ 8-kyu/thinkful-logic-drills-traffic-light/thinkful-logic-drills-traffic-light.py
@@ -0,0 +1,10 @@
+def update_light(current):
+    # Your code here.
+    cycle = {
+        "green" : "yellow",
+        "yellow" : "red",
+        "red" : "green"
+    }
+    
+    return cycle[current]
+    
